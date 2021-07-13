2a01aa9dca78914d40b9a1e00a6f3d7c9ffe574d
Rui Duan <rui.duan@loblaw.ca>
2021-07-13 12:20:10 -0400

    [minigrep] Add search() and its tests; add tests for Config::new()

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,3 +26,51 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+	if line.contains(query) {
+	    results.push(line);
+	}
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_new_two_valid_args() {
+	let args = vec![
+	    String::from("program_name"),
+	    String::from("contains?"),
+	    String::from("filename.txt")];
+
+	let config = Config::new(&args).unwrap();
+
+	assert_eq!("contains?", config.query);
+	assert_eq!("filename.txt", config.filename);
+    }
+
+    #[test]
+    #[should_panic]
+    fn config_new_less_args() {
+	let args = vec!["a".to_string()];
+	let config = Config::new(&args).unwrap();
+    }
+
+    #[test]
+    fn one_result() {
+	let query = "duct";
+	let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+	assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
