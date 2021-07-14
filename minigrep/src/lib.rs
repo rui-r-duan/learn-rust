0c02e5acf4295e36c19a66a0d427d8d8a12f4950
Rui Duan <rui.duan@loblaw.ca>
2021-07-14 11:36:02 -0400

    Use filter() and collect() on iterators to remove state variable

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -56,28 +56,38 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // Removing the mutable state might enable a future enhancement to make
+    // searching happen in parallel, because we wouldn’t have to manage
+    // concurrent access to the results vector.
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
 
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase(); // type: String, not &str slice
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
