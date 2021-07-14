78aa110ef9862c62c896282f944d564a9d5d28e4
Rui Duan <rui.duan@loblaw.ca>
2021-07-14 16:55:25 -0400

    Use Rc<T> to implement multiple references for cons list

--- recursive-type-cons/src/main.rs
+++ recursive-type-cons/src/main.rs
@@ -4,10 +4,26 @@ enum List {
     Nil,
 }
 
+#[derive(Debug)]
+enum RcList {
+    Cons(i32, Rc<RcList>),
+    Nil,
+}
+
 use crate::List::{Cons, Nil};
+use RcList::{Cons as RcCons, Nil as RcNil};
+use std::rc::Rc;
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     println!("{:?}", list);
+
+    let a = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(RcNil)))));
+    let b = RcCons(3, Rc::clone(&a));
+    let c = RcCons(4, Rc::clone(&a));
+
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", c);
 }
