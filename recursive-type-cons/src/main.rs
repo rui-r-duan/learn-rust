40186fed59c89bc77fc15fec9da9dd78f9817a19
Rui Duan <rui.duan@loblaw.ca>
2021-07-14 15:35:10 -0400

    Use derive(Debug) trait to print a cons list

--- recursive-type-cons/src/main.rs
+++ recursive-type-cons/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
@@ -7,4 +8,6 @@ use crate::List::{Cons, Nil};
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!("{:?}", list);
 }
