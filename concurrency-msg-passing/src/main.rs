4b5cca11122c30f451446fae08b3f2148c3dd9de
Rui Duan <rui.duan@loblaw.ca>
2021-07-15 14:23:29 -0400

    Send messages from two threads to main thread

--- concurrency-msg-passing/src/main.rs
+++ concurrency-msg-passing/src/main.rs
@@ -5,12 +5,30 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
             String::from("from"),
             String::from("the"),
             String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+
+        // E0382: borrow of moved value `vals`
+        // println!("val is {:?}", vals);
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("MORE"),
+            String::from("MESSAGES"),
+            String::from("FOR"),
+            String::from("YOU"),
         ];
 
         for val in vals {
