7fe839cddb65e7204deaf1188e135f2e3b6a121f
Rui Duan <rui.duan@loblaw.ca>
2021-07-15 11:56:48 -0400

    Example 2 of thread: use a move closure to take ownership in thread

--- hello-thread/src/main.rs
+++ hello-thread/src/main.rs
@@ -2,6 +2,9 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    //----------------------------------------------------------------
+    // Example 1
+    //----------------------------------------------------------------
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
@@ -13,10 +16,26 @@ fn main() {
     // to complete before continuing to the following loop.
     // handle.join().unwrap();
 
+    //----------------------------------------------------------------
+    // Example 2
+    //----------------------------------------------------------------
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    // without a "move" closure, error E0373
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    // without a "move" closure, the borrow of `v` inside the spawned thread
+    // may reference a non-existed Vec.
+    // drop(v);                    // oh no!
 
     handle.join().unwrap();
 }
