7e9f54ce98f89dec4495e214aa5688527360c503
Rui Duan <rui.duan@loblaw.ca>
2021-07-15 14:19:39 -0400

    Send multiple messages

--- concurrency-msg-passing/src/main.rs
+++ concurrency-msg-passing/src/main.rs
@@ -1,15 +1,28 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-        println!("val is {}", val);
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+
+        // E0382: borrow of moved value `vals`
+        // println!("val is {:?}", vals);
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
