e32f9e2a09bb98b3824ddf5e7c50d990e9935ff2
Rui Duan <rui.duan@loblaw.ca>
2021-07-15 11:43:43 -0400

    First example of thread: thread::spawn() and JoinHandle<()>::join()

---
+++ hello-thread/src/main.rs
@@ -0,0 +1,22 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    // If we put handle.join() here, the main thread will wait for the spawned thread
+    // to complete before continuing to the following loop.
+    // handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
