987575c69f37f1c081446546f222c54c2f2e311d
Rui Duan <rui.duan@loblaw.ca>
2021-07-18 23:01:28 -0400

    Simulate a slow request

--- mini-web-server/src/main.rs
+++ mini-web-server/src/main.rs
@@ -3,6 +3,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::mem;
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -27,8 +29,12 @@ fn handle_connection(mut stream: TcpStream) {
     // let response = "HTTP/1.1 200 OK\r\n\r\n";
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
