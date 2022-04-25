937153b196a6e3aa7f77a5e61dd59b0bccba36ed
Rui Duan <rui.duan@loblaw.ca>
2022-04-25 11:33:29 -0400

    rustfmt restaruant triangle.rs

--- restarurant/src/triangle.rs
+++ restarurant/src/triangle.rs
@@ -1,23 +1,27 @@
 use std::fmt;
 use std::fmt::Result;
-use std::io::Result as IoResult;
 use std::io;
+use std::io::Result as IoResult;
 
 #[derive(Debug)]
 struct Triangle {
     a: f32,
     b: f32,
-    c: f32
+    c: f32,
 }
 
 impl fmt::Display for Triangle {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result {
-	write!(f, "({}, {}, {})", self.a, self.b, self.c)
+        write!(f, "({}, {}, {})", self.a, self.b, self.c)
     }
 }
 
 fn function1() -> String {
-    let pythagorean_triple = Triangle { a: 3.0, b: 4.0, c: 5.0 };
+    let pythagorean_triple = Triangle {
+        a: 3.0,
+        b: 4.0,
+        c: 5.0,
+    };
     format!("{}", pythagorean_triple)
 }
 
