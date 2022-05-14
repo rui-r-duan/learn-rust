00d44f0168863e898c9a66b8791634c7ed185a63
Rui Duan <rui.duan@loblaw.ca>
2022-05-14 18:40:41 -0400

    CLI book example grrs: v2 match Result and pass Ok(content)

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -13,7 +13,14 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let result = std::fs::read_to_string(&args.path);
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("Can't deal with {}, just exit here", error);
+        }
+    };
+    println!("file content: {}", content);
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
