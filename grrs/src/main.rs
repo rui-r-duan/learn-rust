2bd70ff43b7c4d26d5cc8a0b8965cbbd0d31c6c9
Rui Duan <rui.duan@loblaw.ca>
2022-05-14 22:11:41 -0400

    CLI book example grrs: v8 unit test

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -16,12 +16,26 @@ fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{:?}`", &args.path))?;
-    println!("file content: {}", content);
+    // println!("file content: {}", content);
 
+    find_matches(&content, &args.pattern, &mut std::io::stdout())?;
+
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
     for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line)?;
         }
     }
+
     Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
