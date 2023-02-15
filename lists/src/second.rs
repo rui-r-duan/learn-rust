b8e8d69058debee16047ddb299796ca900f3893d
Rui Duan <rui.duan@loblaw.ca>
2023-02-15 18:00:30 -0500

    Add IterMut for singly-linked list
    - Use Option<&mut>::take() to get exclusive access to the mutable ref.
    - Rust understands that it is OK to shard a mutable ref into the
      subfields of the pointed-to-struct, because there is no way to
      "go back up", and they are definitely disjoint.

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -48,6 +48,12 @@ impl<T> List<T> {
             next: self.head.as_deref(),
         }
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
 }
 
 impl<T> Default for List<T> {
@@ -73,6 +79,10 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
@@ -92,6 +102,16 @@ impl<'a, T> Iterator for Iter<'a, T> {
             // self.next = node.next.as_ref().map::<&Node<T>, _>(|node| node);
             self.next = node.next.as_deref();
             &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
         })
     }
 }
@@ -184,5 +204,18 @@ mod tests {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
