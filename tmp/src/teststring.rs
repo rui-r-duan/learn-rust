06efe1760a18a1bafb0724df787429520f57c0fc
Rui Duan <rui.duan@loblaw.ca>
2023-04-19 08:02:17 -0400

    Update program for testing strings

--- tmp/src/teststring.rs
+++ tmp/src/teststring.rs
@@ -4,14 +4,19 @@ fn print_type_of<T>(_: &T) {
 
 fn main() {
     let hello = "Здравствуйте";
-    print_type_of(&hello);
+    print_type_of(&hello); // &str
 
     // error[E0277]: the size for values of type `str` cannot be known at compilation time
-    print_type_of(&"Здравствуйте");
+    // print_type_of("Здравствуйте");
+    print_type_of(&"Здравствуйте"); // &str
 
     let hello = hello.to_string();
-    print_type_of(&hello);
+    print_type_of(&hello); // alloc::string::String
 
-    // let answer = &hello[0];
-    // println!("{}", answer);
+    let answer = hello.get(0..1);
+    println!("{answer:?}"); // None
+
+    let x: String = "3".to_string();
+    let answer = x.get(0..1);
+    println!("{answer:?}"); // Some("3")
 }
