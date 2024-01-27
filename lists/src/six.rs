0cefc1c10dd0a421a39f78f88ab1e5d034a38a4e
Rui Duan <rui.r.duan@gmail.com>
2024-01-27 13:15:57 -0500

    Add Drop, back methods, mut, iter, and into_iter, to LinkedList
    - Drop
    - push_back(), pop_back(), front(), front_mut(), back(), back_mut()
    - iter(), iter_mut(), into_iter()
    - for &'a LinkedList<T> and &'a mut LinkedList<T>: IntoIterator
    - for Iter<'a, T>: Iterator, DoubleEndedIterator, ExactSizeIterator
    - for IterMut<'a, T>: Iterator, DoubleEndedIterator, ExactSizeIterator
    - for LinekdList<T>: IntoIterator
    - for IntoIter<T>: Iterator, DoubleEndedIterator, ExactSizeIterator

--- lists/src/six.rs
+++ lists/src/six.rs
@@ -36,6 +36,24 @@ struct Node<T> {
     elem: T,
 }
 
+pub struct Iter<'a, T> {
+    front: Link<T>,
+    back: Link<T>,
+    len: usize,
+    _boo: PhantomData<&'a T>,
+}
+
+pub struct IterMut<'a, T> {
+    front: Link<T>,
+    back: Link<T>,
+    len: usize,
+    _boo: PhantomData<&'a mut T>,
+}
+
+pub struct IntoIter<T> {
+    list: LinkedList<T>,
+}
+
 impl<T> LinkedList<T> {
     pub fn new() -> Self {
         Self {
@@ -59,14 +77,40 @@ impl<T> LinkedList<T> {
                 (*new.as_ptr()).back = Some(old);
             } else {
                 // If there's no front, then we're the empty list and need
-                // to set the back too.  Also here's some integrity checks
-                // for testing, in case we mess up.
-                debug_assert!(self.back.is_none());
-                debug_assert!(self.front.is_none());
-                debug_assert!(self.len == 0);
+                // to set the back too.
+
+                // Also here's some integrity checks for testing, in
+                // case we mess up.
+                // --> No, remove these `debug_assert`s to make it panic safe.
+                // debug_assert!(self.back.is_none());
+                // debug_assert!(self.front.is_none());
+                // debug_assert!(self.len == 0);
                 self.back = Some(new);
             }
+            // These things always happen!
             self.front = Some(new);
+            self.len += 1;
+        }
+    }
+
+    pub fn push_back(&mut self, elem: T) {
+        unsafe {
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                back: None,
+                front: None,
+                elem,
+            })));
+            if let Some(old) = self.back {
+                // Put the new back before the old one
+                (*old.as_ptr()).back = Some(new);
+                (*new.as_ptr()).front = Some(old);
+            } else {
+                // If there's no back, then we're the empty list and need
+                // to set the front too.
+                self.front = Some(new);
+            }
+            // These things always happen!
+            self.back = Some(new);
             self.len += 1;
         }
     }
@@ -77,14 +121,14 @@ impl<T> LinkedList<T> {
             // Note that we don't need to mess around with `take` anymore
             // because everything is Copy and there are no dtors that will
             // run if we mess up ... right? :) Riiiight? :)))
-	    //
-	    // --> No, it is wrong!  Boxed_node's dtor will be run if
-	    // the debug_assert line panics or the self.len -= 1 line panics!
-	    //
-	    // To maintain the PANIC SAFETY, we must make sure that
-	    // the possible panic points are at the very start or very end of
-	    // the function, ensuring that the invariance-breaking code block
-	    // is in the middle and does not panic.
+            //
+            // --> No, it is wrong!  Boxed_node's dtor will be run if
+            // the debug_assert line panics or the self.len -= 1 line panics!
+            //
+            // To maintain the PANIC SAFETY, we must make sure that
+            // the possible panic points are at the very start or very end of
+            // the function, ensuring that the invariance-breaking code block
+            // is in the middle and does not panic.
             self.front.map(|node| {
                 // Bring the Box back to life so we can move out its value and
                 // Drop it (Box continues to magically understand this for us).
@@ -99,26 +143,246 @@ impl<T> LinkedList<T> {
                 } else {
                     // If the front is now null, then this list is now empty!
 
-		    // The debug_assert! line can panic, and leave self.back
-		    // still pointing to the freed boxed_node, and this could
-		    // result in use-after-free!
-		    // We can move the assert line after self.back = None,
-		    // but a better way is just remove the assert line.
+                    // The debug_assert! line can panic, and leave self.back
+                    // still pointing to the freed boxed_node, and this could
+                    // result in use-after-free!
+                    // We can move the assert line after self.back = None,
+                    // but a better way is just remove the assert line.
 
                     // debug_assert!(self.len == 1); // It can panic!
 
                     self.back = None;
                 }
 
-                self.len -= 1;	// It can panic!
+                self.len -= 1; // It can panic!
                 result
                 // Box gets implicitly freed here, knows there is no T.
             })
         }
     }
 
+    pub fn pop_back(&mut self) -> Option<T> {
+        unsafe {
+            // Only have to do stuff if there is a back node to pop.
+            self.back.map(|node| {
+                // Brint the Box front to life so we can move out its value and
+                // Drop it (Box continues to magically understand this for us).
+                let boxed_node = Box::from_raw(node.as_ptr());
+                let result = boxed_node.elem;
+
+                // Make the next node into the new back.
+                self.back = boxed_node.front;
+                if let Some(new) = self.back {
+                    // Cleanup its reference to the removed node
+                    (*new.as_ptr()).back = None;
+                } else {
+                    // If the back is now null, then this list is now empty!
+                    self.front = None;
+                }
+
+                self.len -= 1;
+                result
+                // Box gets implicitly freed here, knows there is no T.
+            })
+        }
+    }
+
+    pub fn front(&self) -> Option<&T> {
+        unsafe { self.front.map(|node| &(*node.as_ptr()).elem) }
+
+        // The `?` operator does an early return on Option::None.
+        // Does it make our code nicer?
+        // Maybe?  It's kind of a wash for something this simple.  Early returns
+        // are kinda spooky for us, so maybe we should prefer being a bit more
+        // explicit here (I'm sticking to the `map` implementation).
+
+        // unsafe { Some(&(*self.front?.as_ptr()).elem) }
+    }
+
+    // The `&mut` on `self` is optional.  Even `&self` allows returning `&mut` of
+    // the front node's elem.
+    pub fn front_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.front.map(|node| &mut (*node.as_ptr()).elem) }
+    }
+
+    pub fn back(&self) -> Option<&T> {
+        unsafe { self.back.map(|node| &(*node.as_ptr()).elem) }
+    }
+
+    pub fn back_mut(&mut self) -> Option<&T> {
+        unsafe { self.back.map(|node| &(*node.as_ptr()).elem) }
+    }
+
     pub fn len(&self) -> usize {
         self.len
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            front: self.front,
+            back: self.back,
+            len: self.len,
+            _boo: PhantomData,
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            front: self.front,
+            back: self.back,
+            len: self.len,
+            _boo: PhantomData,
+        }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter { list: self }
+    }
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        // Pop until we have to stop
+        while let Some(_) = self.pop_front() {}
+    }
+}
+
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type IntoIter = Iter<'a, T>;
+    type Item = &'a T;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // While self.front == self.back is a tempting condition to check here,
+        // it won't do the right for yielding the last element!  That sort of
+        // thing only works for arrays because of "one-past-the-end" pointers.
+        if self.len > 0 {
+            // We could unwrap front, but this is safer and easier
+            self.front.map(|node| unsafe {
+                self.len -= 1;
+                self.front = (*node.as_ptr()).back;
+                &(*node.as_ptr()).elem
+            })
+        } else {
+            None
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for Iter<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.len > 0 {
+            self.back.map(|node| unsafe {
+                self.len -= 1;
+                self.back = (*node.as_ptr()).front;
+                &(*node.as_ptr()).elem
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T> ExactSizeIterator for Iter<'a, T> {
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<'a, T> IntoIterator for &'a mut LinkedList<T> {
+    type IntoIter = IterMut<'a, T>;
+    type Item = &'a mut T;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // While self.front == self.back is a tempting condition to check here,
+        // it won't do the right for yielding the last element!  That sort of
+        // thing only works for arrays because of "one-past-the-end" pointers.
+        if self.len > 0 {
+            // We could unwrap front, but this is safer and easier
+            self.front.map(|node| unsafe {
+                self.len -= 1;
+                self.front = (*node.as_ptr()).back;
+                &mut (*node.as_ptr()).elem
+            })
+        } else {
+            None
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for IterMut<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.len > 0 {
+            self.back.map(|node| unsafe {
+                self.len -= 1;
+                self.back = (*node.as_ptr()).front;
+                &mut (*node.as_ptr()).elem
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T> ExactSizeIterator for IterMut<'a, T> {
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<T> IntoIterator for LinkedList<T> {
+    type IntoIter = IntoIter<T>;
+    type Item = T;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.into_iter()
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.pop_front()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.list.len, Some(self.list.len))
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.list.pop_back()
+    }
+}
+
+impl<T> ExactSizeIterator for IntoIter<T> {
+    fn len(&self) -> usize {
+        self.list.len
     }
 }
 
